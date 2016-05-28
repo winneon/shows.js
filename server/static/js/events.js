24347ff785b6af090f9dbfbf00886d3433edf0d3
Jesse Bryan <winneonsword@gmail.com>
2016-05-28 09:07:21 -0500

    Added a logout function to the client.

--- server/static/js/events.js
+++ server/static/js/events.js
@@ -9,10 +9,12 @@ socket.on("electron-auth", (url, pathname) => {
 
 		var window = new electron.BrowserWindow({
 			title: "shows.js",
-			width: 500,
+			width: 1000,
 			height: 600,
 			resizable: false,
 			frame: false,
+			show: false,
+			backgroundColor: "#282b30",
 			webPreferences: {
 				nodeIntegration: false
 			}
@@ -20,8 +22,35 @@ socket.on("electron-auth", (url, pathname) => {
 
 		window.loadURL(url);
 
+		window.webContents.on("did-get-redirect-request", (event) => {
+			console.log(window.webContents.getURL());
+		});
+
 		window.webContents.on("did-finish-load", (event) => {
 			var url = window.webContents.getURL();
+			window.show();
+
+			if (url.indexOf("https://discordapp.com/oauth2/authorize") == 0){
+				window.webContents.executeJavaScript(`var interval = setInterval(() => {
+					if (document.getElementsByTagName("footer").length > 0){
+						clearInterval(interval);
+
+						var button = document.createElement("button");
+						var text = document.createTextNode("Logout");
+
+						button.setAttribute("type", "button");
+						button.appendChild(text);
+
+						button.addEventListener("click", (event) => {
+							localStorage.removeItem("token");
+							window.location.reload();
+						});
+
+						var footer = document.getElementsByTagName("footer")[0];
+						footer.insertBefore(button, footer.childNodes[0]);
+					}
+				}, 50);`);
+			}
 
 			if (url.indexOf("/electron_auth?code=") > -1){
 				mainWindow.loadURL(url.replace("electron_auth", "authenticate") + "&electron=1&next=" + pathname);
